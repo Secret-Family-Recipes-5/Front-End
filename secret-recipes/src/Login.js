372680e6e217e96f730ac3985cf99d052ac9ea95
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-22 09:34:36 -0700

    setup most of structure

--- secret-recipes/src/Login.js
+++ secret-recipes/src/Login.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
 
+  return (
+    <div className='formContainer'>
+      <form>
+        <input
+          type='text'
+          name='username'
+          placeholder='Username'
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password'
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Login
---
+++ secret-recipes/src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+
+  return (
+    <div className='navContainer'>
+
+    </div>
+  );
+}
+
+export default Nav
