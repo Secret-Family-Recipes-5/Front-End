131aae6ccdf0a924aee8b93b9be298c35b22ba6c
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-22 09:50:41 -0700

    set up login form

--- secret-recipes/src/App.js
+++ secret-recipes/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom'
+import Login from './Login'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
+      <Login />
+
+
       <Switch>
+
+        <Route path='/profile'>
+
+        </Route>
+        <Route path='/register'>
+
+        </Route>
+        <Route path='/'>
+
+        </Route>
 
       </Switch>
     </div>
--- secret-recipes/src/Login.js
+++ secret-recipes/src/Login.js
@@ -2,19 +2,49 @@ import React, { useState } from 'react'
 
 const Login = () => {
 
+  const initialFormValues = {
+    username: '',
+    password: '',
+  }
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const handleInput = (event) => {
+    const { name, value } = event.target
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    // Axios PUT
+  }
+
   return (
     <div className='formContainer'>
-      <form>
+      <form onSubmit={handleSubmit} >
+
         <input
           type='text'
           name='username'
           placeholder='Username'
+          value={formValues.username}
+          onChange={handleInput}
         />
+
         <input
           type='password'
           name='password'
           placeholder='Password'
+          value={formValues.password}
+          onChange={handleInput}
         />
+
+        <button>Login</button>
       </form>
     </div>
   );
